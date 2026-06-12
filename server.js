503475870faebbc1630f8fff6fd60380a88c6a8d
shri-honeytha <shrihoneythareddy2006@gmail.com>
2026-06-12 11:16:12 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from "express"
+import cors from "cors"
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+const PORT=process.env.port || 5000
+app.listen(PORT,()=>{
+    console.log("Server Started")
+})
+
+const taskSchema=mongoose.Schema(
+    {
+        task:String,
+    }
+)
+
+const taskModel=mongoose.model("Task",taskSchema)
+
+app.post("/tasks",async(req,res)=>{
+    const task=await taskModel.create({task:req.body.task})
+    res.json(task)
+})
+app.get("/tasks",async(req,res)=>{
+    const tasks=await taskModel.find()
+    res.json(tasks)
+})
+app.delete("/tasks/:id",async(req,res)=>{
+    await taskModel.findByIdAndDelete(req.params.id)
+    res.json({message:"Task Deleted"})
+})
+
+
+
