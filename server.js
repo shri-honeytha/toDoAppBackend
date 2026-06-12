5e5d34a6b7d47ef910d369181f98d0506c3518fd
shri-honeytha <shrihoneythareddy2006@gmail.com>
2026-06-12 11:19:25 +0530

    port to PORT

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 mongoose.connect(process.env.MONGO_URI)
-const PORT=process.env.port || 5000
+const PORT=process.env.PORT || 5000
 app.listen(PORT,()=>{
     console.log("Server Started")
 })
